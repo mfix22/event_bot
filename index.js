a503d206f62a6f6951e3fc6004137ab074490119
Michael Fix <mfix@wisc.edu>
2016-02-20 03:36:51 -0600

    test

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
-var pg = require('pg');
+// var pg = require('pg');
+var bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -16,21 +19,28 @@ app.get('/', function(request, response) {
 });
 
 app.post('/event', function(request, response) {
-	var text = request.body.text;
-	response.send(text);
+	console.log("+++++++++++++++++++++++++++++++++++++" + req.body + "+++++++++++++++++++++++++++++++++++++")
+    response.send("Complete");
 });
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-})
+
+app.post('/api/users', function(req, res) {
+    var user_id = req.body.id;
+
+    res.send(user_id);
+});
+
+// app.get('/db', function (request, response) {
+//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//     client.query('SELECT * FROM test_table', function(err, result) {
+//       done();
+//       if (err)
+//        { console.error(err); response.send("Error " + err); }
+//       else
+//        { response.render('pages/db', {results: result.rows} ); }
+//     });
+//   });
+// })
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
