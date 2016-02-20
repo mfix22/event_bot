ecfa92bfe1f57eff0bcb46b8eccbeea6706eaf83
Michael Fix <mfix@wisc.edu>
2016-02-20 09:14:56 -0600

    refactor methods

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ app.post('/event', function(request, response) {
 	  	campaign_id = body.id;
 
 	  	//edit email
-		edit_email(campaign_id, info, mom)
+		edit_email(campaign_id, info, mom, insert_button)
 	});
 
     response.send(output);
@@ -110,7 +110,7 @@ app.post('/event', function(request, response) {
 /*************************
  * edit campaign content *
  *************************/
-function edit_email(campaign_id, info, mom){
+function edit_email(campaign_id, info, mom, insert_button){
 
 	var options = {method: 'PUT',
 	  					url: 'https://us10.api.mailchimp.com/3.0/campaigns/'+campaign_id.trim()+'/content',
