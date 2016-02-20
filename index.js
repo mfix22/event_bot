f27d4ce97db92a5561ff57081a3178c0c0262188
Michael Fix <mfix@wisc.edu>
2016-02-20 05:33:04 -0600

    test

--- index.js
+++ index.js
@@ -41,12 +41,12 @@ app.post('/event', function(request, response) {
 		output += ("When: *" + info[2] + "*")		//date
 	}
 	if (info.length >= 4){
-		var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
-    		'((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
-    		'((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
-    		'(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
-    		'(\?[;&a-z\d%_.~+=-]*)?'+ // query string
-    		'(\#[-a-z\d_]*)?$','i'); // fragment locater
+		vvar pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+  			'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+  			'((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  			'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+  			'(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+  			'(\\#[-a-z\\d_]*)?$','i'); // fragment locater
  		if(!pattern.test(info[3])) {
     		output += (" *" + info[3] + "*")
   		}
