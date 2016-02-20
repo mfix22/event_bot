200c474035d5cfddaed45f218749eb321a6ed458
Michael Fix <mfix@wisc.edu>
2016-02-20 07:01:01 -0600

    add mailchimp put

--- index.js
+++ index.js
@@ -54,7 +54,6 @@ app.post('/event', function(request, response) {
 					//time
 	}
 	if (info.length >= 5){
-		date_info += info[3];
 		insert_button = '<table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnButtonContentContainer" style="border-collapse: separate !important;border-radius: 3px;background-color: #2BAADF;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">\n                    <tbody>\n                        <tr>\n                            <td align="center" valign="middle" class="mcnButtonContent" style="font-family: Arial;font-size: 16px;padding: 15px;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">\n                                <a class="mcnButton " title="SIGN UP HERE" href="http://' + info[4] + '" target="_blank" style="font-weight: bold;letter-spacing: normal;line-height: 100%;text-align: center;text-decoration: none;color: #FFFFFF;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;display: block;">SIGN UP HERE</a>\n                            </td>\n                        </tr>\n                    </tbody>\n                </table>\n            </td>\n        </tr>\n    </tbody>\n'
 		output += ("\nRSVP: <http://" + info[4] + "|LINK>");
 	}
