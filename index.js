af508aa6f0c81905bfe58bbb7848e500a3d8918f
Michael Fix <mfix@wisc.edu>
2016-02-22 22:09:42 -0600

    fix team icons

--- index.js
+++ index.js
@@ -54,20 +54,19 @@ app.post('/event', function(request, response) {
 	}
 	if (info.length >= 4){
 		// var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-  // 			'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
-  // 			'((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-  // 			'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-  // 			'(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-  // 			'(\\#[-a-z\\d_]*)?$','i'); // fragment locater
- 	// 	// if(!pattern.test(info[3])) {
+  		// '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+  		// '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  		// '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+  		// '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+  		// '(\\#[-a-z\\d_]*)?$','i'); // fragment locater
+ 		// 	if(!pattern.test(info[3])) {
     		// output += (" *" + info[3].trim() + "*")
     		// date_info += info[3].trim();
-  		}
+  			// }
   		// else{
 		insert_button = '<table border="0" cellpadding="0" cellspacing="0" width="100%" class="mcnButtonContentContainer" style="border-collapse: separate !important;border-radius: 3px;background-color: #2BAADF;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">\n                    <tbody>\n                        <tr>\n                            <td align="center" valign="middle" class="mcnButtonContent" style="font-family: Arial;font-size: 16px;padding: 15px;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">\n                                <a class="mcnButton " title="SIGN UP HERE" href="http://' + info[3].trim() + '" target="_blank" style="font-weight: bold;letter-spacing: normal;line-height: 100%;text-align: center;text-decoration: none;color: #FFFFFF;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;display: block;">SIGN UP HERE</a>\n                            </td>\n                        </tr>\n                    </tbody>\n                </table>\n            </td>\n        </tr>\n    </tbody>\n'
 		output += ("\nRSVP: <http://" + info[3].trim() + "|LINK>");
   		// }
-					//time
 	}
 	
 	mom =  moment(date_info, 'D/MM/YY h:mma'); 
